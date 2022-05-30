e6f6ac82a33a6928714a0652b08379f0ec2c2f5e
foupp <viggonarvaez27@hotmail.com>
2022-05-29 22:38:42 -0400

    Added tsconfig and updated index.d.ts to match module and target

--- index.d.ts
+++ index.d.ts
@@ -10,31 +10,11 @@
 declare class QuickBoard {
     constructor(options: {
         max: number,
-        data: Array<T>,
+        data: Array<any>,
         map: (...args: any[]) => void,
-        reverse: boolean = false
-    }) {
-        if (!options) throw new Error('options is required.');
-        if (!options.max) throw new Error('options.max is required.');
-        if (!options.data) throw new Error('options.data is required.');
-        if (!options.map) throw new Error('options.map is required.');
-        if (!options.sort) throw new Error('options.sort is required.');
-
-        if (typeof options.max !== 'number') throw new Error('options.max must be a number.');
-        if (!Array.isArray(options.data)) throw new Error('options.data must be an array.');
-        if (typeof options.map !== 'function') throw new Error('options.map must be a function.');
-        if (typeof options.sort !== 'function') throw new Error('options.sort must be a function.');
-        if (options.reverse && typeof options.reverse !== 'boolean') throw new Error('options.reverse must be a boolean.');
-
-        if (options.max < 1) throw new Error('options.max must be greater than 0.');
-        if (options.data.length < 1) throw new Error('options.data length must be greater than 0.');
-
-        this.set('max', options.max);
-        this.set('data', options.data);
-        this.set('map', options.map);
-        this.set('sort', options.sort);
-        if (options.reverse) this.set('reverse', options.reverse);
-    };
+        sort: (...args: any[]) => void,
+        reverse?: boolean
+    });
 
     /**
      * @public
@@ -49,36 +29,27 @@ declare class QuickBoard {
      * 5. Discord#0000 1123 messages
      * ```
      */
-    create(): string {
-        return this.init();
-    };
+    create(): string;
 
     /**
      * @private
      * Initializes the the leaderboard creation
      */
-    init(): string {
-        if (this.get('reverse')) return this.template().split('\n').reverse().join('\n');
-        else return this.template();
-    };
+    init(): string;
 
     /**
      * @private
      * Formats data created on new QuickBoard()
      * @returns string
      */
-    template() {
-        return this.get('data').slice(0, this.get('max')).sort(this.get('sort')).map(this.get('map')).join('\n');
-    };
+    template(): string;
 
     /**
      * @private
      * @param {string} key Index to search for in class 'QuickBoard'
-     * @returns this[key]
+     * @returns {any} this[key]
      */
-    get(key: string) {
-        return this[key];
-    };
+    get(key: string): any;
 
     /**
      * @private
@@ -86,9 +57,7 @@ declare class QuickBoard {
      * @param {string | Array | number | boolean | ((...args: any[]) => void)} value New value for said 'index'
      * @returns {any} New value
      */
-    set(key: string, value: string | Array | number | boolean | ((...args: any[]) => void)): any {
-        return this[key] = value;
-    };
-};
+    set(key: string, value: string | Array<any> | number | boolean | ((...args: any[]) => void)): any;
+}
 
 export default QuickBoard;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+<reference types="typescript" />
+
 class QuickBoard {
 
   /**
